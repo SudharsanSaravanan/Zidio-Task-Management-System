d11abffa23bf515066898f81013a39a3561d1276
thatguywhocode <138510462+thatguywhocode@users.noreply.github.com>
2025-03-05 22:05:38 +0530

    made login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login.jsx"; 
 
 function App() {
   return (
-    <div className="App">
-      hi
-    </div>
+    <Router>
+      <Routes>  
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+      </Routes>
+    </Router>
   );
 }
 
